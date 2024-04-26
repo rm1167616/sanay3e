e4a42b7b32626f2348df3e4be99e7de848305913
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-04-26 12:22:39 +0300

    ss

--- routes/category/createcategory.js
+++ routes/category/createcategory.js
@@ -2,18 +2,15 @@ const router = require("express").Router();
 const { body } = require("express-validator");
 const connection = require("../../db/dbConnection");
 const util = require("util"); // helper 
-const fs = require('fs');
-const upload = require("../../middleware/uploadimage");
 
 
-// api create category
-router.post("/", async (req, res) => {
-    const { name, description, package, min_time, material , img ,public_id } = req.body;
+
+
+router.post("/",async(req, res) => {
     try {
+        const { name , description , img , public_id , min_time , package , material} = req.body ;
         const query = util.promisify(connection.query).bind(connection);
 
-
-        uploadImageToCloudinary(img);
         // prepare the object 
         const category = {
             name: name,
