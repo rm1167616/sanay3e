5026e691af786a49a29be4d3b090fea903c818e8
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-04-26 16:31:15 +0300

    mennaghazal

--- routes/support/Support.js
+++ routes/support/Support.js
@@ -2,40 +2,31 @@ const router = require("express").Router();
 const { body } = require("express-validator");
 const connection = require("../../db/dbConnection");
 const util = require("util");
- router.post("/",async (req, res) => {
+router.post("/", async (req, res) => {
   try
   {
 
     const { username, email, description } = req.body;
-  
- 
-    const user = 'SELECT id FROM user WHERE username = ? AND email = ?';
-    connection.query(user, [username, email], (err, results) => {
-      if (err) {
-        res.status(500).json({ error: 'Internal server error' });
-        return;
-      }
-  
-      
-      if (results.length > 0) {
-        const userId = results[0].id;
-  
+    const query = util.promisify(connection.query).bind(connection); 
+    const user = await query(" select * from user where email = ? ",email);
+    if(user[0])
+    {
+      // prepare the object 
+      const userid = user.id;
+      const supportobj = {
+        user_id : userid ,
+        description : description 
+      };
+      // insert it in data base 
+      const supp = await query (" insert into support set ?" , supportobj);
+      res.status(200).json(supportobj);
+    }
+    else
+    {
+      res.status(404).json("sorry the user not exist ....");
+    }
     
-        const support = 'INSERT INTO support (description, user_id) VALUES (?, ?)';
-        connection.query(support, [description, userId], (err, results) => {
-          if (err) {
-            console.error('Error inserting support info:', err);
-            res.status(500).json({ error: 'Internal server error' });
-            return;
-          }
-         
-          res.status(200).json({ message: 'Support information saved successfully' });
-        });
-      } else {
        
-        res.status(404).json({ error: 'Error checking username or email' });
-      }
-    });
   }
     catch(err)
     {
