50aada454c8b146cf2a7befc2d6681805ce5d8b0
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-05-05 18:33:04 +0300

    login.js

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,42 +1,49 @@
 const router = require("express").Router();
-const connection = require(../../db/dbconnection);
-const { body, validationResult } = require('express-validator');
+const connection = require("../../db/dbConnection");
+const { body , validationResult } = require('express-validator');
 const util = require("util"); // helper 
 const bcrypt = require("bcrypt");
-
+const crypto = require("crypto");
 
 // ========= LOGIN ==============//
 
-router.post("/", async (req, res) => {
-   const { email, password } = req.body;
-   try {
-      //=== validation of request 
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-         return res.status(400).json({ errors: errors.array() })
-      }
-      //==== check the email is exist or not ====//
-      const query = util.promisify(connection.query).bind(connection) // transform query to promise to can use await/ async
-      const user = await query("SELECT * FROM user WHERE email = ?", [email]);
-      const checkpassword = await bcrypt.compare(
-         password, user[0].password
-      );
-      if (user[0] == null) {
-         res.status(404).json({ msg: "the email or password not found....." })
-      }
+router.post("/",body("email").isEmail().withMessage("enter valid email"),
+                     body("password"),async (req,res)=>{
+    try
+    {
+       //=== validation of request 
+     const errors = validationResult(req);
+     if(!errors.isEmpty())
+     {
+        return res.status(400).json({errors:errors.array()})
+     }
+     //==== check the email is exist or not ====//
+     const query = util.promisify(connection.query).bind(connection) // transform query to promise to can use await/ async
+     const user = await query ("select * from user where email = ? ",[req.body.email])
+     if(user.length == 0)
+     {
+        res.status(404).json({msg:"the email or password not found....."})
+     }
       // === compare hashed password ===// 
-      else if (checkpassword) {
+      const checkpassword = await bcrypt.compare(
+         req.body.password,
+         user[0].password
+      );
+      if(checkpassword)
+      {
          res.status(200).json(user);
       }
-      else {
+      else
+      {
          res.status(404).json({
-            msg: "the password or email not correct"
+            msg:"the password or email not correct"
          })
       }
-   }
-   catch (err) {
-      res.send(err)
-   }
+     }
+    catch(err)
+    {
+        res.send(err)
+    }
 })
 
-module.exports = router;
+module.exports = router
