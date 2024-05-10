55933dfb79ab3ee2fe9c104f556a9a6dde2c1711
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-05-10 16:54:20 +0300

    ss

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use("/location",require("./routes/auth/location"));
 app.use("/createcategory",require("./routes/category/createcategory"));
 app.use("/getcategory",require("./routes/category/getallcategory"));
 app.use("/updatecategory",require("./routes/category/updatecategory"));
+app.use("/deletecategory",require("./routes/category/deletecategory"));
 //============== profile function ========// 
 app.use("/ChangePassword",require("./routes/customer/ChangePassword"));
 app.use("/EditProfile",require("./routes/customer/EditProfile"));
---
+++ routes/category/deletecategory.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { body } = require("express-validator");
+const connection = require("../../db/dbConnection");
+const util = require("util"); // helper 
+const fs = require("fs");
+const upload = require("../../middleware/uploadimage");
+
+
+router.delete("/:id",async(req,res)=>{
+    try
+    {
+        const query = util.promisify(connection.query).bind(connection);
+        const categoryid = req.params.id;
+        const cate = await query("SELECT * FROM category WHERE id = ?", categoryid);
+        // check if categry exist or not
+        if(cate[0])
+        {
+            fs.unlinkSync(`uploads/${cate[0].img}`);
+            fs.unlinkSync(`uploads/${cate[0].coverImg}`);
+            await query (" delete from category where id = ?",categoryid);
+            res.status(200).json(" THE CATEGORY DELETED.....") ;
+        }
+        else
+        {
+            res.status(404).json("sorry the category not exist...");
+        }
+
+    }
+    catch(err)
+    {
+        res.status(404).json(err)
+    }
+})
+
+
+module.exports = router ;
