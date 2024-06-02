4b5190f35a0ca2cd41f2f85a75ca2a16f9545e56
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-06-02 18:08:24 +0300

    arwa

--- routes/recommendation/price_prediction.js
+++ routes/recommendation/price_prediction.js
@@ -18,20 +18,44 @@ router.post('/', async (req, res) => {
         });
 
         let predictedCharge = response.data.predicted_charge_amount;
+        // Predicted charge before tax and material cost
+        const predictedChargeBefore = predictedCharge;
+        // Apply taxes (35% including 20% tax)
+        const taxAmount = predictedCharge * 0.35;
+        predictedCharge += taxAmount;
 
-        // If addMaterial flag is true, add 50% to the predicted charge
+        // Material cost addition
+        let materialCost = 0;
         if (addMaterial === true) {
-            predictedCharge *= 1.5;
+            materialCost = predictedCharge * 0.5;
+            predictedCharge += materialCost;
         }
 
-        // Apply taxes (35% including 20% tax)
-        predictedCharge *= 1.35;
+        
 
-        // Round the predicted charge amount to the nearest integer
-        predictedCharge = Math.round(predictedCharge);
+        
+
+        // Admin fees (25% of predicted charge after taxes and material costs)
+        const adminFees = predictedCharge * 0.25;
+        const predictedChargeAfterAdminFees = predictedCharge - (adminFees+taxAmount);
+
+        // Round the values to the nearest integer
+        const roundedPredictedChargeBefore = Math.round(predictedChargeBefore);
+        const roundedPredictedCharge = Math.round(predictedCharge);
+        const roundedMaterialCost = Math.round(materialCost);
+        const roundedTaxAmount = Math.round(taxAmount);
+        const roundedAdminFees = Math.round(adminFees);
+        const roundedPredictedChargeAfterAdminFees = Math.round(predictedChargeAfterAdminFees);
 
         console.log('Python API Response:', response.data);
-        res.json({ predicted_charge_amount: predictedCharge });
+        res.json({
+            predicted_charge_amount_before: roundedPredictedChargeBefore,
+            predicted_charge_amount_after: roundedPredictedCharge,
+            material_cost: roundedMaterialCost,
+            tax_amount: roundedTaxAmount,
+            admin_fees: roundedAdminFees,
+            craftsman_predicted_price: roundedPredictedChargeAfterAdminFees
+        });
     } catch (error) {
         console.error('Error predicting charge amount:', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
