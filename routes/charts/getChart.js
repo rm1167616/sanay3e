5ecdf2c4ac267ad509c39f240e3b79e2d01ab19f
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-06-07 16:10:15 +0300

    pla pla

--- routes/charts/getChart.js
+++ routes/charts/getChart.js
@@ -6,29 +6,16 @@ const util = require("util"); // helper
 
 router.get("/:id", async (req, res) => {
     try {
-
-
-        // calculate current date 
-        const now = new Date();
-
-        // Get the components of the date
-        const year = now.getFullYear();
-        const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-        const day = String(now.getDate()).padStart(2, '0');
-
-        // Format the date as YYYY-MM-DD
-        const formattedDate = `${year}-${month}-${day}`;
-
-
-
-
         const craftsmanid = req.params.id;
         const query = util.promisify(connection.query).bind(connection);
-        const chartsobj = await query(`SELECT DATE_FORMAT(scadule.date, '%Y-%m-%e') AS formatted_date, booking.price
-FROM scadule
-JOIN booking ON booking.scaduleid = scadule.id
-WHERE scadule.craftsmanid = ?
-`, [craftsmanid, formattedDate]);
+        const chartsobj = await query(`
+            SELECT scadule.date, booking.price
+            FROM scadule
+            JOIN booking ON booking.scaduleid = scadule.id
+            WHERE scadule.craftsmanid = ?
+              AND DATE(scadule.date) BETWEEN DATE(DATE_SUB(NOW(), INTERVAL WEEKDAY(NOW()) DAY)) AND DATE(DATE_SUB(NOW(), INTERVAL WEEKDAY(NOW()) - 6 DAY))
+        `, [craftsmanid]);
+        
         res.status(200).json(chartsobj);
 
     }
