235af3891a13bd943a4dd1a66a99d43694c24fa0
RaDwan <119738657+rm1167616@users.noreply.github.com>
2024-06-08 15:55:19 +0300

    lol

--- routes/gets/getreviewandrate.js
+++ routes/gets/getreviewandrate.js
@@ -8,6 +8,7 @@ router.get("/:id", async (req, res) => {
     try {
         const query = util.promisify(connection.query).bind(connection);
         const craftsmanid = req.params.id;
+        
         const object = await query(`
         SELECT 
     user.username,
@@ -19,9 +20,16 @@ JOIN
     user ON user.id = reviews.customerid
 WHERE 
 reviews.craftsmanid = ?`, craftsmanid)
-        
+if(object)
+        {
             res.status(200).json(object);
 
+        }
+        else
+        {
+            res.status(200).json("sorry the craftsman dont have any comment");
+        }
+           
     }
     catch (err) {
         res.status(404).json(err)
